a107cf0eabf3044339a9427f312fd9e347c5db9d
Jadenchen <menzxisland@gmail.com>
2016-07-19 17:09:42 +0800

    modify

--- single_list.c
+++ single_list.c
@@ -2,119 +2,146 @@
 #include <stdlib.h>
 #include <assert.h>
 
-typedef struct _List{
-	struct _List *next;
-	int val;
+typedef struct _List {
+    struct _List *next;
+    int val;
 } List;
 
 
-List **head = NULL;
+void print_list(List *head)
+{
+	if (head == NULL)
+		printf("head is null\n");
+    assert(head);
+    List *curr = head;
 
-List * New(int a){
+    while(curr) {
+        printf("%d\n", curr->val);
+        curr = curr->next;
+    }
 
-	List *tmp = (List *)malloc(sizeof(List));
-	if (tmp == NULL){
-		printf("can not malloc \n");
-		return NULL;
-	}
-	tmp-> val = a;
-	tmp-> next = NULL;
-	return tmp;
-}
-
-
-void print(List **head){
-
-	assert(head);
-	List *curr = *head;
-	while(curr){
-		printf("val %d\n", curr->val);
-		curr = curr->next;
-	}
-}
-
-int swap (List **head, List *a, List *b){
-
-	assert(head);
-	assert(a);
-	assert(b);
-	/*head = a or b */
-	if (a == *head || b == *head){
-		List *nohead = (a = *head) ? b:a;
-		List *curr = *head;
-
-		for (;curr->next != nohead; curr = curr->next);
-		curr->next = *head;
-		curr = nohead->next;
-		nohead->next = (*head)->next; 
-		(*head)->next = curr;
-		*head = nohead;
-		return 0;
-	}
-
-
-	List *prev_a = *head;
-	List *prev_b = *head;
-
-	for(; prev_a->next != a; prev_a = prev_a->next);
-	for(; prev_b->next != b; prev_b = prev_b->next);
-
-	prev_a ->next = b;
-	prev_b ->next = a;
-
-	prev_a = b->next;
-	b->next = a->next;
-	a->next = prev_a;
-	return 0;
 
 }
 
-int bubblesort(List **head)
+int swap (List **head, List *a, List *b)
 {
 
-	assert(head);
+    assert(head);
+    assert(a);
+    assert(b);
+    /*head = a or b */
+    if (a == *head || b == *head) {
+        List *nohead = (a = *head) ? b:a;
+        List *curr = *head;
 
-	List *tmp = *head;
+        for (; curr->next != nohead; curr = curr->next);
+        curr->next = *head;
+        curr = nohead->next;
+        nohead->next = (*head)->next;
+        (*head)->next = curr;
+        *head = nohead;
+        return 0;
+    }
 
-	while (tmp){
-		List *tmp1 = tmp->next;
-		while(tmp1){
-			if(tmp->val > tmp1->val){
-				swap(head, tmp, tmp1);
-			}
-			tmp1 = tmp1->next;
-		}
 
-		tmp = tmp->next;
-	}
+    List *prev_a = *head;
+    List *prev_b = *head;
 
-	return 0;
+    for(; prev_a->next != a; prev_a = prev_a->next);
+    for(; prev_b->next != b; prev_b = prev_b->next);
+
+    prev_a ->next = b;
+    prev_b ->next = a;
+
+    prev_a = b->next;
+    b->next = a->next;
+    a->next = prev_a;
+    return 0;
+
+}
+
+int bubble_sort(List **head)
+{
+
+    assert(head);
+
+    List *tmp = *head;
+
+    while (tmp) {
+        List *tmp1 = tmp->next;
+        while(tmp1) {
+            if(tmp->val > tmp1->val) {
+                swap(head, tmp, tmp1);
+            }
+            tmp1 = tmp1->next;
+        }
+
+        tmp = tmp->next;
+    }
+
+    return 0;
+}
+
+List *list_append (List **list, int val)
+{
+    List *tmp = (List *)malloc(sizeof(List));
+    if (tmp == NULL) {
+        printf("can not create memory \n");
+        return NULL;
+    }
+
+    tmp-> val = val;
+    tmp-> next = NULL;
+
+    List *curr = *list;
+
+	/*chech head is null */
+    if (curr == NULL) {
+        curr = tmp;
+        return tmp;
+    }
+
+    //r(; curr==MNULLcurr= curr->next);
+    for(;curr->next != NULL; curr = curr->next);
+
+    curr->next = tmp;
+	
+    return *list;
 }
 
 
-int main (void){
+int main (int argc, char *argv[])
+{
+
+    int test_count = atoi(argv[1]);
+    char *test_input = argv[2];
+
+    FILE *file = fopen(test_input, "r");
+
+    List *list = NULL;
+
+    int num;
+
+    int i;
+    for(i = 0; i < test_count; i++) {
+        fscanf(file, "%d", &num);
+        list = list_append(&list, num);
+    }
+
+  	fclose(file);
+
+    bubble_sort(&list);
+
+    print_list(list);
 	
-	List *tmp0 = New(0);
-	List *tmp1 = New(1);
-	List *tmp2 = New(2);
-	List *tmp3 = New(3);
 
-	tmp0->next = tmp1;
-	tmp1->next = tmp2;
-	tmp2->next = tmp3;
 	
-	head = &tmp0;
-	
-	print(head);
+/*	int i;
+	List *list = NULL;
+	for (i = 0; i < 5; i++)
+		list = list_append(&list, i);
 
-	swap(head, tmp0, tmp1);
-	printf("swap \n");
-
-	print(head);
-
-	printf("bubble sort\n");
-	bubblesort(head);
-	print(head);
-
-	return 0;
+	print_list(list);
+*/
+    return 0;
 }
